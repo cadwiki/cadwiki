9395c5053925dd453b74cc8417414cd767608d2b
cadwiki <99513485+cadwiki@users.noreply.github.com>
2022-10-30 11:06:37 -0700

    add new-post.ps1 to create new posts from template

---
+++ WebApplication/new-post.ps1
@@ -0,0 +1,21 @@
+﻿$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+$templateFolder = $scriptPath
+$postFolder = "$templateFolder\BlogApp\wwwroot\posts"
+$mdFile = "post-template.md"
+$jsonFile = "post-template.json"
+
+Set-Location -Path $postFolder -PassThru
+$countFiles = Get-ChildItem .\* -Recurse | where {$_.extension -in ".json"} 
+write-host ("found {0} post json files in directory." -f $countFiles.Count)
+
+Copy-Item "$templateFolder\$mdFile" -Destination "$postFolder"
+Copy-Item "$templateFolder\$jsonFile" -Destination "$postFolder"
+write-host "copied new template files."
+
+$content = Get-Content $postFolder\$jsonFile
+$json = $content | ConvertFrom-Json
+$json.DateCreated = (Get-Date).ToString()
+$json.Id = $countFiles.Count.ToString()
+
+$json | ConvertTo-Json | Set-Content -Path $postFolder\$jsonFile
