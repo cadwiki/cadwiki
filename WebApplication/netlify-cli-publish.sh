fef04156a462270e6e7a66b1c1dce9758100308b
cadwiki <99513485+cadwiki@users.noreply.github.com>
2022-12-06 15:32:11 -0800

    remove logic that creates post manifest from sh script

--- WebApplication/netlify-cli-publish.sh
+++ WebApplication/netlify-cli-publish.sh
@@ -1,8 +1,3 @@
-cd ./BlogApp/wwwroot/posts
-sh -c 'ls -d -- "$@" > post.manifest' sh *.json
-cd ../../..
-## Bash add pause prompt for 5 seconds ##
-## read -t 5 -p "waiting 5 seconds..."
 cd ./BlogApp/
 netlify deploy --build
 read -p "Press any key to resume ..."
